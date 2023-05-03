7f5cb0b8165a2de75530bfacd03474b452419e40
UoYOllie <oj613@york.ac.uk>
2023-05-03 09:34:01 +0100

    adding things

---
+++ scripts/architecture.js
@@ -0,0 +1,40 @@
+function archOne(){
+    document.getElementById("image-holder").innerHTML =
+        "<p>[ARCH1] Assessment 1 ‘Final Product UML Diagram’ - Modified Structural Diagram</p>" +
+        "<img src='assets/images/architecture/arch1.png'>";
+}
+function archTwo(){
+    document.getElementById("image-holder").innerHTML =
+        "<p>[ARCH2] Assessment 1 ‘Game UML’ - Modified Structural Diagram</p>" +
+        "<img src='assets/images/architecture/arch3.png'>";
+}
+function archThree(){
+    document.getElementById("image-holder").innerHTML =
+        "<p>[ARCH3] Assessment 1 ‘Product Use Case UML’ - Modified Product Use Case</p>" +
+        "<img src='assets/images/architecture/arch3.png'>" +
+        "<p>[ARCH3a] Assessment 1 Controls - Chef Changes - Added</p>" +
+        "<img src='assets/images/architecture/arch3a.png'>" +
+        "<p>[ARCH3b] Assessment 1 Controls - Menu - Added</p>" +
+        "<img src='assets/images/architecture/arch3b.png'>" +
+        "<p>[ARCH3c] Assessment 1 Controls - Movement - Added</p>" +
+        "<img src='assets/images/architecture/arch3c.png'>" +
+        "<p>[ARCH3d] Assessment 1 Controls - Interactions - Added</p>" +
+        "<img src='assets/images/architecture/arch3d.png'>";
+}
+function archFour(){
+    document.getElementById("image-holder").innerHTML =
+        "<p>[ARCH4a] Assessment 1 Sequence - Burger Recipe - Added</p>" +
+        "<img src='assets/images/architecture/arch4a.png'>" +
+        "<p>[ARCH4b] Assessment 1 Sequence - Salad Recipe - Added</p>" +
+        "<img src='assets/imags/architecture/arch5b.png'";
+}
+function archFive(){
+    document.getElementById("image-holder").innerHTML =
+        "<p>[ARCH5] Final Product UML - refactoring classes for easier extension Assessment 2.</p>" +
+        "<img src='assets/images/architecture/arch5.png'>";
+}
+function archSix(){
+    document.getElementById("image-holder").innerHTML =
+        "<p>[ARCH6] Final Product UML - planning extension for Assessment 2 requirements. </p>" +
+        "<img src='assets/images/architecture/arch6.png'>";
+}
